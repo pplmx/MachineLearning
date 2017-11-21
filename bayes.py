8d6607257024e9f26b439dfa8141bf356d033c49
mystic-cg <215104920@qq.com>
2017-11-21 21:16:21 +0800

    bayes is starting

---
+++ bayes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by PyCharm
+# @author  : mystic
+# @date    : 2017/11/21 20:40
+"""
+    naive bayes classifier
+    优点:
+        在数据较少的情况下仍然有效,可以处理多类别问题
+    缺点:
+        对于输入数据的准备方式较为敏感
+    适用数据类型:
+        标称型数据
+"""
--- learning.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Created by PyCharm
-# @author  : mystic
-# @date    : 2017/11/13 20:04
-"""
-    Machine Learning
-"""
-
