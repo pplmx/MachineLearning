ec730621cc7cedea9568929057dc4d29736a3218
mystic-cg <215104920@qq.com>
2018-03-21 21:37:52 +0800

    support vector machine

--- svm.py
+++ svm.py
@@ -21,3 +21,32 @@
         [6]使用算法: 几乎所有分类问题都可以使用SVM,值得一提的是,SVM本身是一个二类分类器
                     对多类问题应用,SVM需要对代码做一些修改
 """
+import random
+
+
+def load_data_set(file_name):
+    data_mat = []
+    label_mat = []
+    fr = open(file_name)
+    for line in fr.readlines():
+        line_arr = line.strip().split('\t')
+        data_mat.append([float(line_arr[0]), float(line_arr[1])])
+        label_mat.append(float(line_arr[2]))
+    return data_mat, label_mat
+
+
+def select_jrand(i, m):
+    j = i
+    while j==i:
+        j = int(random.uniform(0, m))
+    return j
+
+
+def clip_alpha(aj, H, L):
+    if aj > H:
+        aj = H
+    if L > aj:
+        aj = L
+    return aj
+
+
