296c13ac2de06b6c9fc1117d5159e44218c2d54d
mystic-cg <215104920@qq.com>
2018-03-23 21:45:38 +0800

    the result is not what I expect

--- svm.py
+++ svm.py
@@ -42,11 +42,11 @@ def select_j_rand(i, m):
     return j
 
 
-def clip_alpha(aj, h, l):
-    if aj > h:
-        aj = h
-    if l > aj:
-        aj = l
+def clip_alpha(aj, high, low):
+    if aj > high:
+        aj = high
+    if low > aj:
+        aj = low
     return aj
 
 
@@ -64,7 +64,7 @@ def simple_smo(data_list, class_label_list, constant, tolerate, max_loop):
     label_mat = mat(class_label_list).transpose()
     b = 0
     m, n = shape(data_mat)
-    alpha_mat = mat(zeros(m, 1))
+    alpha_mat = mat(zeros((m, 1)))
     loop = 0
     while loop < max_loop:
         alpha_pairs_changed = 0
@@ -75,9 +75,50 @@ def simple_smo(data_list, class_label_list, constant, tolerate, max_loop):
                       ((label_mat[i] * e_i > tolerate) and (alpha_mat[i] > 0))
             if is_okay:
                 j = select_j_rand(i, m)
+                f_x_j = float(multiply(alpha_mat, label_mat).T * (data_mat * data_mat[j, :].T)) + b
+                e_j = f_x_j - float(label_mat[j])
+                alpha_i_old = alpha_mat[i].copy()
+                alpha_j_old = alpha_mat[j].copy()
+                if label_mat[i] != label_mat[j]:
+                    low = max(0, alpha_mat[j] - alpha_mat[j])
+                    high = min(constant, constant + alpha_mat[j] - alpha_mat[i])
+                else:
+                    low = max(0, alpha_mat[j] + alpha_mat[j] - constant)
+                    high = min(constant, alpha_mat[j] + alpha_mat[i])
+                if low == high:
+                    print('low == high')
+                    continue
+                eta = 2.0 * data_mat[i, :] * data_mat[j, :].T - (data_mat[i, :] * data_mat[i, :].T) - (
+                        data_mat[j, :] * data_mat[j, :].T)
+                if eta >= 0:
+                    print('eta >= 0')
+                    continue
+                alpha_mat[j] -= label_mat[j] * (e_i - e_j) / eta
+                alpha_mat[j] = clip_alpha(alpha_mat[j], high, low)
+                if (abs(alpha_mat[j]) - alpha_j_old) < 0.00001:
+                    print('j not moving enough')
+                    continue
+                alpha_mat[i] += label_mat[j] * label_mat[i] * (alpha_j_old - alpha_mat[j])
+                b1 = b - e_i - label_mat[i] * (alpha_mat[i] - alpha_i_old) * data_mat[i, :] * data_mat[i, :].T - \
+                     label_mat[j] * (alpha_mat[j] - alpha_j_old) * data_mat[i, :] * data_mat[j, :].T
+                b2 = b - e_j - label_mat[i] * (alpha_mat[i] - alpha_i_old) * data_mat[i, :] * data_mat[j, :].T - \
+                     label_mat[j] * (alpha_mat[j] - alpha_j_old) * data_mat[j, :] * data_mat[j, :].T
+                if (0 < alpha_mat[i]) and (constant > alpha_mat[i]):
+                    b = b1
+                elif (0 < alpha_mat[j]) and (constant > alpha_mat[j]):
+                    b = b2
+                else:
+                    b = (b1 + b2) / 2.0
+                alpha_pairs_changed += 1
+                print('loop: %d, i: %d, pairs changed: %d' % (loop, i, alpha_pairs_changed))
+            if alpha_pairs_changed == 0:
+                loop += 1
+            else:
+                loop = 0
+            print('loop number: %d' % loop)
+        return b, alpha_mat
 
 
 if __name__ == "__main__":
     data_arr, label_arr = load_data_set('resource/testSet1.txt')
-    print(data_arr)
-    print(label_arr)
+    simple_smo(data_arr, label_arr, 0.6, 0.001, 40)
