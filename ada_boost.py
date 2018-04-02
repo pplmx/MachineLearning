9b2e6be2a72e58adba42ff61bd7c1609a461cd01
mystic-cg <215104920@qq.com>
2018-04-02 19:45:33 +0800

    recognize hand writing

---
+++ ada_boost.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by PyCharm
+# @author  : mystic
+# @date    : 2018/4/2 19:10
+
+"""
+    AdaBoost
+优点:
+    泛化错误率低,易编码,可以应用在大部分分类器上,无参数调整
+缺点:
+    对离群点敏感
+适用数据类型:
+    数值型和标称型数据
+"""
