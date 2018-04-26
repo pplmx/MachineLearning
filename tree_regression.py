241b80574d7c56277dbfbf9dc0ec23b3aa541c47
mystic-cg <215104920@qq.com>
2018-04-26 21:31:08 +0800

    tree regression

--- tree_regression.py
+++ tree_regression.py
@@ -32,32 +32,32 @@ def load_data_set(filename):  # general function to parse tab -delimited floats
         return data_list
 
 
-def bin_split_data_set(data_set, feature, value):
+def bin_split_data_set(data_mat, feature, value):
     # TODO
     # before change: will get index 0 of array
     # after change: not get index 0 of array
-    mat_0 = data_set[nonzero(data_set[:, feature] > value)[0], :]
-    mat_1 = data_set[nonzero(data_set[:, feature] <= value)[0], :]
+    mat_0 = data_mat[nonzero(data_mat[:, feature] > value)[0], :]
+    mat_1 = data_mat[nonzero(data_mat[:, feature] <= value)[0], :]
     return mat_0, mat_1
 
 
-def regression_leaf(data_set):  # returns the value used for each leaf
-    return mean(data_set[:, -1])
+def regression_leaf(data_mat):  # returns the value used for each leaf
+    return mean(data_mat[:, -1])
 
 
-def regression_err(data_set):
-    return var(data_set[:, -1]) * shape(data_set)[0]
+def regression_err(data_mat):
+    return var(data_mat[:, -1]) * shape(data_mat)[0]
 
 
-def choose_best_split(data_set, leaf_type=regression_leaf, err_type=regression_err, ops=(1, 4)):
+def choose_best_split(data_mat, leaf_type=regression_leaf, err_type=regression_err, ops=(1, 4)):
     tolerance_split = ops[0]
     tolerance_n = ops[1]
     # if all the target variables are the same value: quit and return value
-    if len(set(data_set[:, -1].T.tolist()[0])) == 1:  # exit condition 1
-        return None, leaf_type(data_set)
-    m, n = shape(data_set)
+    if len(set(data_mat[:, -1].T.tolist()[0])) == 1:  # exit condition 1
+        return None, leaf_type(data_mat)
+    m, n = shape(data_mat)
     # the choice of the best feature is driven by Reduction in RSS error from mean
-    split = err_type(data_set)
+    split = err_type(data_mat)
     best_split = inf
     best_idx = 0
     best_val = 0
@@ -65,8 +65,8 @@ def choose_best_split(data_set, leaf_type=regression_leaf, err_type=regression_e
         # TODO
         # before change: data_set[:, feature_idx]
         # after change: data_set[:, feature_idx].T.tolist()[0]
-        for split_val in set(data_set[:, feature_idx].T.tolist()[0]):
-            mat_0, mat_1 = bin_split_data_set(data_set, feature_idx, split_val)
+        for split_val in set(data_mat[:, feature_idx].T.tolist()[0]):
+            mat_0, mat_1 = bin_split_data_set(data_mat, feature_idx, split_val)
             if (shape(mat_0)[0] < tolerance_n) or (shape(mat_1)[0] < tolerance_n):
                 continue
             new_s = err_type(mat_0) + err_type(mat_1)
@@ -76,27 +76,27 @@ def choose_best_split(data_set, leaf_type=regression_leaf, err_type=regression_e
                 best_split = new_s
     # if the decrease (split-best_split) is less than a threshold don't do the split
     if (split - best_split) < tolerance_split:  # exit condition 2
-        return None, leaf_type(data_set)
-    mat_0, mat_1 = bin_split_data_set(data_set, best_idx, best_val)
+        return None, leaf_type(data_mat)
+    mat_0, mat_1 = bin_split_data_set(data_mat, best_idx, best_val)
     if (shape(mat_0)[0] < tolerance_n) or (shape(mat_1)[0] < tolerance_n):  # exit condition 3
-        return None, leaf_type(data_set)
+        return None, leaf_type(data_mat)
     # returns the best feature to split on
     # and the value used for that split
     return best_idx, best_val
 
 
-def create_tree(data_set, leaf_type=regression_leaf, err_type=regression_err, ops=(1, 4)):
+def create_tree(data_mat, leaf_type=regression_leaf, err_type=regression_err, ops=(1, 4)):
     # assume data_set is NumPy Mat so we can array filtering
     # choose the best split
-    feature, val = choose_best_split(data_set, leaf_type, err_type, ops)
+    feature, val = choose_best_split(data_mat, leaf_type, err_type, ops)
     # if the splitting hit a stop condition return val
     if feature is None:
         return val
-    return_tree = {'split_idx': feature, 'split_val': val}
-    left_set, right_set = bin_split_data_set(data_set, feature, val)
-    return_tree['left'] = create_tree(left_set, leaf_type, err_type, ops)
-    return_tree['right'] = create_tree(right_set, leaf_type, err_type, ops)
-    return return_tree
+    tree = {'split_idx': feature, 'split_val': val}
+    left_set, right_set = bin_split_data_set(data_mat, feature, val)
+    tree['left'] = create_tree(left_set, leaf_type, err_type, ops)
+    tree['right'] = create_tree(right_set, leaf_type, err_type, ops)
+    return tree
 
 
 if __name__ == '__main__':
